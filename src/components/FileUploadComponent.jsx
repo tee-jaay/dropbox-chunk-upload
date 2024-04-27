6dd7fd512489de8cc031edb40e1f9e189a17cf4f
Tee Jaay <tamjid84@yahoo.com>
2024-04-27 22:03:55 +0600

    hide notification

--- src/components/FileUploadComponent.jsx
+++ src/components/FileUploadComponent.jsx
@@ -1,11 +1,13 @@
 "use client";
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Dropbox } from 'dropbox';
 const ACCESS_TOKEN = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
 const FileUploadComponent = () => {
+    const fileInputRef = useRef(null);
     const [file, setFile] = useState(null);
     const [sessionId, setSessionId] = useState(null);
     const [isActive, setIsActive] = useState(false);
+    const [showNotification, setShowNotification] = useState(false);
     const [isSuccess, setIsSuccess] = useState(false);
     const [isError, setIsError] = useState(false);
     const [isProgress, setIsProgress] = useState(false);
@@ -14,11 +16,13 @@ const FileUploadComponent = () => {
 
     const handleFileChange = (event) => {
         const selectedFile = event.target.files[0];
-        setFile(selectedFile);
-        if (file) {
-            setIsActive(true);
-            console.log(file);
-        }
+        setFile(() => {
+            if (selectedFile) {
+                setIsActive(true);
+                console.log(selectedFile);
+            }
+            return selectedFile;
+        });
     };
     const dbx = new Dropbox({ accessToken: ACCESS_TOKEN });
     const setSessionIdCall = async () => {
@@ -27,10 +31,8 @@ const FileUploadComponent = () => {
             contents: '',
         });
         setSessionId(sessionStart.result.session_id);
-        console.log('setSessionIdCall', sessionId);
         return;
     }
-
     const handleUpload = async (e) => {
         setIsActive(false);
         e.preventDefault();
@@ -40,12 +42,10 @@ const FileUploadComponent = () => {
         };
         sessionChunkUpload();
     };
-
     const sessionChunkUpload = async () => {
         setIsProgress(true);
         const CHUNK_SIZE = 5 * 1024 * 1024;
         const reader = new FileReader();
-
         const fileData = await new Promise((resolve, reject) => {
             reader.onload = () => {
                 resolve(reader.result);
@@ -55,7 +55,6 @@ const FileUploadComponent = () => {
             };
             reader.readAsArrayBuffer(file);
         });
-
         const totalChunks = Math.ceil(file.size / CHUNK_SIZE);
         for (let i = 0; i < totalChunks; i++) {
             const chunk = fileData.slice(i * CHUNK_SIZE, (i + 1) * CHUNK_SIZE);
@@ -69,7 +68,6 @@ const FileUploadComponent = () => {
             });
             const chunkProgress = (chunk.byteLength / file.size) * 100;
             setProgress((prevProgress) => prevProgress + chunkProgress);
-            console.log("progress # ", progress);
         }
         var uploadFinished = await dbx.filesUploadSessionFinish({
             cursor: {
@@ -81,22 +79,35 @@ const FileUploadComponent = () => {
                 path: '/tests/' + file.name,
             }
         });
-
         setIsSuccess(true);
-
         reader.readAsArrayBuffer(file);
-
         if (!uploadFinished?.result?.id) {
             setIsError(true);
+            setShowNotification(true);
         } else {
             setUploadedFileId(uploadFinished?.result?.id);
+            setShowNotification(true);
         }
         setIsProgress(false);
+        setProgress(0);
+        setTimeout(() => {
+            setShowNotification(false);
+            setIsError(false);
+            setIsSuccess(false);
+            resetFileInput();
+        }, 3000);
     }
+
+
+    const resetFileInput = () => {
+        if (fileInputRef.current) {
+            fileInputRef.current.value = null;
+        }
+        setFile(null);
+    };
 
     useEffect(() => {
         setSessionIdCall();
-        console.log("Progress updated:", progress);
     }, [progress]);
     return (
         <div className="container">
@@ -104,8 +115,10 @@ const FileUploadComponent = () => {
                 <div className="col-12 py-2">
                     <div style={{ height: '10px' }}>
                         <div className={isProgress ? 'd-block' : 'd-none'}>
-                            <div className="progress" role="progressbar" aria-label="Warning example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-                                <div className="progress-bar text-bg-warning" style={{ width: `${progress}%` }}>{progress}%</div>
+                            <div className="progress" role="progressbar" aria-label="file upload progress" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">
+                                <div className="progress-bar progress-bar" style={{ width: `${progress}%` }}>
+                                    {(file && progress) && `${(file.size * (progress / 100) / (1024 * 1024)).toFixed(2)}MB`}
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -115,18 +128,20 @@ const FileUploadComponent = () => {
                             <button onClick={handleUpload} className={`btn btn-primary ${isActive ? '' : 'disabled'}`} >Upload</button>
                         </form>
                     </div>
-                    <div>
-                        {isSuccess &&
-                            <div className="alert alert-success" role="alert">
-                                {file && `${file.name} uploaded: ${uploadedFileId}`}
-                            </div>
-                        }
-                        {isError &&
-                            <div className="alert alert-danger" role="alert">
-                                Error occured!
-                            </div>
-                        }
-                    </div>
+                    {
+                        showNotification && <div>
+                            {isSuccess &&
+                                <div className="alert alert-success" role="alert">
+                                    {file && `${file.name} uploaded: ${uploadedFileId}`}
+                                </div>
+                            }
+                            {isError &&
+                                <div className="alert alert-danger" role="alert">
+                                    Error occured!
+                                </div>
+                            }
+                        </div>
+                    }
                 </div>
             </div>
         </div>
